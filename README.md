eb3e07b562d86331635e5bc819afb475ed43f732
n-del <nbahmanyar@zoho.com>
2018-04-12 17:48:05 -0400

    fixed readme and improved report_gen

--- README.md
+++ README.md
@@ -1,22 +1,25 @@
-# logs project
+# Logs Project
 Source code for a report generator written in Python and connecting to
 PostgresSQL article log database. Three reports are output: the most popular articles,
 the most popular authors, and an error report. Each report is output to a separate
 text file.
 By: Navid Bahmanyar
 
+## DEPENDENCIES
 
-MODULES
+- psycopg2: PostgresSQL Python package
 
-1. report_gen.py: outputs reports
-
-
-HOW TO USE:
+## SETUP
 
 1. Navigate to the vagrant directory
 2. Run "vagrant up" to start the VM
 3. Run "vagrant ssh" to log in
-4. Clone my repository: https://github.com/nbahmanyar/logs_proj.git
-5. Navigate to the repository in bash
-6. Run "python report_gen.py"
-7. The three reports will be output as text files in the same directory
+4. Setup the database by running "cd /vagrant" followed by
+    "psql -d news -f newsdata.sql"
+5. Clone my repository: https://github.com/nbahmanyar/logs_proj.git
+
+## USAGE
+
+1. Navigate to the cloned repository
+2. Run "python3 report_gen.py"
+3. The three reports will be output as text files in the same directory
--- report_gen.py
+++ report_gen.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python3
 # This is a Python script used to generate some reports on the news database
 
 import psycopg2
@@ -18,7 +19,7 @@ def write_article_report(conn, outfile):
     """
     sql_popular_articles = '''
     SELECT    title, access_count
-    FROM      articles INNER JOIN 
+    FROM      articles INNER JOIN
               (SELECT path, count(*) AS access_count
               FROM log
               WHERE status = '200 OK'
@@ -38,14 +39,14 @@ def write_author_report(conn, outfile):
     """
     sql_popular_authors = '''
     SELECT    authors.name, sum(access_count) as views
-    FROM      articles 
-              INNER JOIN 
+    FROM      articles
+              INNER JOIN
               (SELECT path, count(*) AS access_count
               FROM log
               WHERE status = '200 OK'
               GROUP BY path) AS access ON
                     access.path LIKE '%'||articles.slug
-              INNER JOIN 
+              INNER JOIN
               authors on
                 articles.author = authors.id
     GROUP BY  authors.name
@@ -61,22 +62,21 @@ def write_error_report(conn, outfile):
     """ Output a report detailing days in which there were a lot of errors
     """
     sql_errors = '''
-    SELECT      CAST(time AS DATE) AS day, 
-                ROUND(100 * 
-                      SUM(CASE WHEN status='404 NOT FOUND' THEN 1 ELSE 0 END) / 
-                      SUM(CASE WHEN status='200 OK' THEN 1 ELSE 0 END), 1) AS err
-    FROM        log 
-    GROUP BY    day 
-    HAVING      ROUND(100 * 
-                      SUM(CASE WHEN status='404 NOT FOUND' THEN 1 ELSE 0 END) / 
-                      SUM(CASE WHEN status='200 OK' THEN 1 ELSE 0 END), 1) > 1
-    ORDER BY    err DESC 
+    select to_char(date, 'FMMonth FMDD, YYYY'),
+           to_char(err/total * 100, 'FM999.00') as ratio
+        from (select time::date as date,
+              count(*) as total,
+              sum((status != '200 OK')::int)::float as err
+              from log
+              group by date) as errors
+    where err/total > 0.01
+    order by ratio desc
     ;
-    '''
+    '''  # improvement per Udacity grader's revisions
     results = fetch_results(conn, sql_errors)
     with open(outfile, "w") as f:
         for item in results:
-            f.write('{:%B %d, %Y} -- {}% errors\n'.format(item[0], item[1]))
+            f.write('{} -- {}% errors\n'.format(item[0], item[1]))
 
 
 # Run the script -- connect to the PostgresSQL DB and output the three reports
